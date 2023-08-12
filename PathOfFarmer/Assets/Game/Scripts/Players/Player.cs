da0eb972114de9ca203f9a04033788b16d2c8b3c
KipoGo <vit-tan@mail.ru>
2023-08-12 17:32:42 +0300

    Update class

--- PathOfFarmer/Assets/Game/Scripts/Players/Player.cs
+++ PathOfFarmer/Assets/Game/Scripts/Players/Player.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace Assets.Game.Scripts.Players
 {
     public class Player
@@ -5,6 +7,23 @@ namespace Assets.Game.Scripts.Players
         public Player()
         {
 
+        }
+
+        public PlayerView PlayerView { get; set; }
+    }
+
+    public class Mover
+    {
+        private readonly Player _player;
+
+        public Mover(Player player)
+        {
+            _player = player;
+        }
+
+        public void Move()
+        {
+            _player.PlayerView.transform.position += new Vector3();
         }
     }
 }
