a05ab8e357b51da25b9789c67c2410ed3ddf873c
KipoGo <vit-tan@mail.ru>
2023-08-12 17:08:01 +0300

    Add class
    Enty point

---
+++ PathOfFarmer/Assets/Game/Scripts/EntryPoint.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class EntryPoint: MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        
+    }
+}
