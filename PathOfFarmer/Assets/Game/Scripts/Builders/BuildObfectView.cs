52cf8a39a83764073ac639715c8cb0a59fec7228
KipoGo <vit-tan@mail.ru>
2023-08-13 13:34:45 +0300

    Update

--- PathOfFarmer/Assets/Game/Scripts/Builders/BuildObfectView.cs
+++ PathOfFarmer/Assets/Game/Scripts/Builders/BuildObfectView.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 namespace Assets.Game.Scripts.Builders
 {
+    [RequireComponent(typeof(Collider),typeof(Rigidbody))]
     public class BuildObfectView : MonoBehaviour, IBuildObject
     {
         [SerializeField, FoldoutGroup("Components")] private Renderer _renderer;
