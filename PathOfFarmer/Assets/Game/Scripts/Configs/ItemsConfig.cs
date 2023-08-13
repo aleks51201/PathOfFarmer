a86fc76a6d6b4a8302e1647f3da8a1a5e552b7ca
KipoGo <vit-tan@mail.ru>
2023-08-13 18:12:55 +0300

    Update
    templates

---
+++ PathOfFarmer/Assets/Game/Scripts/Configs/ItemsConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Game.Scripts.Configs
+{
+    [CreateAssetMenu(fileName = "ItemsConfig", menuName = "SO/ItemsConfig")]
+    public class ItemsConfig : ScriptableObject
+    {
+        [SerializeField] private ItemData[] _items;
+
+        public ItemData[] Items => _items;
+    }
+
+    [Serializable]
+    public struct ItemData
+    {
+        public Sprite Icon;
+        public GameObject Prefab;
+    }
+}
